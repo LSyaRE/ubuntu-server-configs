cd38a8db53a0e0b609e930588ce487d412552041
LSyaRE <jonasvillacis@yahoo.com>
2025-03-05 19:51:49 -0500

    fix execution bug

--- minimal_installation.sh
+++ minimal_installation.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 init() {
-    docker volume create portainer_data
-    docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
-    docker compose up -d 
+    sudo docker volume create portainer_data
+    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+    sudo docker compose up -d 
 }
 
 init
